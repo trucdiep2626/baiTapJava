9403f56cf206ccacfc17c68fe13ae81d92a2cab4
Your Name <trucdiep2626@gmail.com>
2020-09-09 21:31:56 +0700

    bai 5

--- baitapmang/src/bai7/bai7.java
+++ baitapmang/src/bai7/bai7.java
@@ -1,25 +1,30 @@
-package bai5;
+package bai7;
 
 import java.util.Scanner;
 
-public class bai5 {
+public class bai7 {
     public static void main(String[] args) {
+        //7. Sử dụng mảng 1 chiều nhập vào dãy số nguyên gồm n phần tử.
+        // In ra phần tử MAX, MIN và số thứ tự của chúng trong dãy
         Scanner sc = new Scanner(System.in);
         System.out.println("nhap n : ");
         int n = sc.nextInt();
         int[] c = new int[n];
         taoMang(c, n);
         int k = sc.nextInt();
-        System.out.print(" so o vi tri " + k + " trong mang : " + c[k]);
     }
-
     public static void taoMang(int[] c, int n) {
         Scanner sc = new Scanner(System.in);
 
         for(int i = 0; i < n; ++i) {
             c[i] = sc.nextInt();
         }
+    }
+    public static void inMang(int[] c, int n) {
+        Scanner sc = new Scanner(System.in);
 
+        for(int i = 0; i < n; ++i) {
+            c[i] = sc.nextInt();
+        }
     }
 }
-
