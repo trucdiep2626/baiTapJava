c9b57d3884f4bf9001e56459212f30390e3b749f
Your Name <trucdiep2626@gmail.com>
2020-11-30 15:59:56 +0700

    update

---
+++ Buoi13/src/bai8/word.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bai8;
+
+/**
+ *
+ * @author admin
+ */
+public class word {
+    private int id;
+    private String en;
+
+    public word() {
+    }
+    private String vn;
+
+    @Override
+    public String toString() {
+        return "word{" + "id=" + id + ", en=" + en + ", vn=" + vn + '}';
+    }
+
+    public word(int id, String en, String vn) {
+        this.id = id;
+        this.en = en;
+        this.vn = vn;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEn() {
+        return en;
+    }
+
+    public void setEn(String en) {
+        this.en = en;
+    }
+
+    public String getVn() {
+        return vn;
+    }
+
+    public void setVn(String vn) {
+        this.vn = vn;
+    }
+    
+}
